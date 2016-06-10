9bf928d4f10ea2f83c2e0f8d08fde8c4383a58f2
Rainer Sieger <rsieger@pangaea.de>
2016-06-10 17:55:47 +0200

    macdeploy - Qt 5.6.0 -> Qt 5.6.1

--- macdeploy.sh
+++ macdeploy.sh
@@ -2,12 +2,12 @@
 echo
 echo PanConverter
 
-echo - macdeployqt 
+echo - macdeployqt
 
 cd ~/Development/GitHub/PanConverter
 
 rm -R '../../Distribution/PanConverter/PanConverter.app'
-cp -R './build-PanConverter-Desktop_Qt_5_6_0_clang_64bit-Release/PanConverter.app' '../../Distribution/PanConverter/PanConverter.app'
+cp -R './build-PanConverter-Desktop_Qt_5_6_1_clang_64bit-Release/PanConverter.app' '../../Distribution/PanConverter/PanConverter.app'
 cp './trunk/Resources/Info.plist' '../../Distribution/PanConverter/PanConverter.app/Contents/Info.plist'
 
 /Developer/Qt/5.6/clang_64/bin/macdeployqt '../../Distribution/PanConverter/PanConverter.app'
